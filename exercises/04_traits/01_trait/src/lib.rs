e3bac0da3778256dbddee1504512da1b9d561e3e
Enver KEKA <enver.keka@kaizen-solutions.net>
2024-12-03 10:40:42 +0100

    Exercice 4 - Trait Complete

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -1,8 +1,20 @@
 // Define a trait named `IsEven` that has a method `is_even` that returns a `true` if `self` is
 // even, otherwise `false`.
+pub trait IsEven {
+    fn is_even(self) -> bool;
+}
 //
 // Then implement the trait for `u32` and `i32`.
-
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0u32
+    }
+}
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0i32
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
