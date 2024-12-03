70fa34427375207e05366d6f75b1eed6069a5aeb
Enver KEKA <enver.keka@kaizen-solutions.net>
2024-12-03 16:43:48 +0100

    Exercice 4 - Drop Complete

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -1,7 +1,28 @@
 // TODO: implement a so-called "Drop bomb": a type that panics when dropped
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
+pub struct DropBomb{
+    difused : bool
+}
 
+impl DropBomb{
+    pub fn new() -> DropBomb{
+        DropBomb{
+            difused : false
+        }
+    }
+    pub fn defuse(&mut self) -> (){
+        self.difused = true;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.difused {
+            panic!()
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
